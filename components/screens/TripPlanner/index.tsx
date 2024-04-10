5a5e07f325c4171b88b6bbb5b98e6d0c15422e84
Andrew Payne <andrewpayne64@gmail.com>
2024-04-10 13:31:41 -0400

    google maps

--- components/screens/TripPlanner/index.tsx
+++ components/screens/TripPlanner/index.tsx
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Dimensions,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import MapView, { Marker } from "react-native-maps";
+import MapViewDirections from "react-native-maps-directions";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { colors } from "../../../theme";
 import { ParkDetail } from "../../../types/schemas";
 
@@ -19,6 +16,10 @@ export default function TripPlannerScreen({
 }: TripPlannerScreenProps) {
   const [parks, setParks] = useState<ParkDetail[]>();
   const [loading, setLoading] = useState(true);
+  const [destination, setDestination] = useState<any>(null);
+  const [origin, setOrigin] = useState<any>(null);
+
+  console.log("destination", destination);
 
   const getParks = async () => {
     try {
@@ -39,39 +40,93 @@ export default function TripPlannerScreen({
   }, []);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       {loading ? (
         <View style={styles.loadingContainer}>
           <ActivityIndicator size="large" color={colors.copperBrown} />
         </View>
       ) : parks ? (
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: Number(parks[0].latitude),
-            longitude: Number(parks[0].longitude),
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-        >
-          {parks.map((park) => {
-            return (
-              <Marker
-                key={park.id}
-                coordinate={{
-                  latitude: Number(park.latitude),
-                  longitude: Number(park.longitude),
-                }}
+        <View style={styles.container}>
+          <View style={styles.searchContainer}>
+            {/* Origin search */}
+            <GooglePlacesAutocomplete
+              placeholder="Origin"
+              fetchDetails={true}
+              onPress={(data, details = null) => {
+                if (details && details.geometry && details.geometry.location) {
+                  setOrigin({
+                    latitude: details.geometry.location.lat,
+                    longitude: details.geometry.location.lng,
+                  });
+                }
+              }}
+              query={{
+                key: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+                language: "en",
+              }}
+              debounce={200}
+              styles={styles.autocompleteContainer}
+            />
+            {/* Destination search */}
+            <GooglePlacesAutocomplete
+              placeholder="Destination"
+              fetchDetails={true}
+              onPress={(data, details = null) => {
+                if (details && details.geometry && details.geometry.location) {
+                  setDestination({
+                    latitude: details.geometry.location.lat,
+                    longitude: details.geometry.location.lng,
+                  });
+                }
+              }}
+              query={{
+                key: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+                language: "en",
+              }}
+              debounce={200}
+              styles={styles.autocompleteContainer}
+            />
+          </View>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: Number(parks[0].latitude),
+              longitude: Number(parks[0].longitude),
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            {parks.map((park) => {
+              return (
+                <Marker
+                  key={park.id}
+                  identifier={park.id}
+                  coordinate={{
+                    latitude: Number(park.latitude),
+                    longitude: Number(park.longitude),
+                  }}
+                  title={park.name}
+                />
+              );
+            })}
+            {process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY && (
+              <MapViewDirections
+                origin={origin}
+                destination={destination}
+                apikey={process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY}
+                strokeWidth={3}
+                strokeColor="blue"
+                mode={"DRIVING"}
               />
-            );
-          })}
-        </MapView>
+            )}
+          </MapView>
+        </View>
       ) : (
         <View>
           <Text>No parks found.</Text>
         </View>
       )}
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -80,13 +135,25 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: "100%",
-    height: "100%",
+    flex: 1,
   },
   loadingContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    height: Dimensions.get("window").height,
+  },
+  searchContainer: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+    right: 10,
+    zIndex: 1,
+  },
+  autocompleteContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "gray",
   },
 });
